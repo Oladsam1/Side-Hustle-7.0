f4c1ae1bfc9e8367a4ab0acde067983f6ae1d983
Oladsam1 <Oladsa1>
2023-03-11 12:01:09 +0100

    Added a new PHP file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Task One</title>
+</head>
+<body>
+	<?php
+$name =  "My name is Abdulsamad";
+echo $name;
+?>
+</body>
+</html>
